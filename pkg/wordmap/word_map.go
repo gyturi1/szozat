135bfa3a3dc013db959756643fea215050ebb31b
Gyula Turi <gyturi@gmail.com>
2022-02-08 15:34:20 +0100

    update word list

--- pkg/wordmap/word_map.go
+++ pkg/wordmap/word_map.go
@@ -1,59 +1,51 @@
 package wordmap
 
 import (
-	"bufio"
 	"embed"
-
-	"github.com/gyturi1/szozat/pkg/lib"
+	"encoding/json"
+	"fmt"
+	"strings"
 )
 
-type e struct{}
-type wordmap map[string]e
-
-//go:embed words.txt
+//go:embed words.json
 var f embed.FS
 
-//193676 is the line count of the words.txt
-const wordmapsize = 193_676
+var m map[string]struct{}
+var empty struct{}
 
-var m wordmap
-
-func Filter(ws []lib.Word) []lib.Word {
-	var ret []lib.Word = make([]lib.Word, 0)
-	for _, w := range ws {
-		if containsWord(w) {
-			ret = append(ret, w)
-		}
-	}
-	return ret
-}
-
-func containsWord(w lib.Word) bool {
-	return contains(w.String())
-}
-
-func contains(w string) bool {
+func Contains(w string) bool {
 	_, ok := m[w]
 	return ok
 }
 
 func init() {
 	if m == nil {
-		m = make(wordmap, wordmapsize)
-		f, err := f.Open("words.txt")
+		c, err := f.ReadFile("words.json")
 		if err != nil {
-			panic(err)
-		}
-		defer f.Close()
-
-		sc := bufio.NewScanner(f)
-
-		for sc.Scan() {
-			m[string(sc.Bytes())] = e{}
+			fmt.Println("can not init words map from words.json")
+			return
 		}
 
-		if sc.Err() != nil {
-			panic(sc.Err())
+		m, err = Read(c)
+		if err != nil {
+			fmt.Println("can not init words map from words.json")
+			return
 		}
 	}
+}
+
+func Read(c []byte) (map[string]struct{}, error) {
+	var ret map[string]struct{} = make(map[string]struct{})
+	var words [][]string
+
+	err := json.Unmarshal(c, &words)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, w := range words {
+		ret[strings.Join(w, "")] = empty
+	}
+
+	return ret, nil
 }
--- pkg/wordmap/word_map_test.go
+++ pkg/wordmap/word_map_test.go
@@ -1,48 +1,36 @@
 package wordmap
 
-import (
-	"reflect"
-	"testing"
+import "testing"
 
-	"github.com/gyturi1/szozat/pkg/lib"
-)
-
-func TestFilter(t *testing.T) {
+func Test_Contains(t *testing.T) {
+	type args struct {
+		w string
+	}
 	tests := []struct {
 		name string
-		ws   []lib.Word
-		want []lib.Word
+		args args
+		want bool
 	}{
 		{
-			name: "T1",
-			ws:   []lib.Word{{"a", "l", "a", "t", "t"}},
-			want: []lib.Word{{"a", "l", "a", "t", "t"}},
+			name: "Empty",
+			args: args{w: ""},
+			want: false,
 		},
 		{
-			name: "T2",
-			ws:   []lib.Word{{"t", "ü", "cs", "ö", "k"}},
-			want: []lib.Word{{"t", "ü", "cs", "ö", "k"}},
+			name: "Contains",
+			args: args{w: "lándzsa"},
+			want: true,
 		},
 		{
-			name: "T3",
-			ws:   []lib.Word{{"t", "ü", "cs", "ö", "k"}, {"a", "l", "a", "t", "t"}},
-			want: []lib.Word{{"t", "ü", "cs", "ö", "k"}, {"a", "l", "a", "t", "t"}},
-		},
-		{
-			name: "T4",
-			ws:   []lib.Word{{"t", "ü", "cs", "ö", "k"}, {"a", "l", "a", "t", "t"}, {"l", "á", "z"}},
-			want: []lib.Word{{"t", "ü", "cs", "ö", "k"}, {"a", "l", "a", "t", "t"}},
-		},
-		{
-			name: "T5 nyári is definitly a 4 letter word see: github.com/gyturi1/cleanwords/README.md for details",
-			ws:   []lib.Word{{"t", "ü", "cs", "ö", "k"}, {"a", "l", "a", "t", "t"}, {"l", "á", "z"}, {"n", "y", "á", "r", "i"}},
-			want: []lib.Word{{"t", "ü", "cs", "ö", "k"}, {"a", "l", "a", "t", "t"}, {"n", "y", "á", "r", "i"}},
+			name: "Not Contains",
+			args: args{w: "kottyo"},
+			want: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Filter(tt.ws); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Filter() = %v, want %v", got, tt.want)
+			if got := Contains(tt.args.w); got != tt.want {
+				t.Errorf("Contains() = %v, want %v", got, tt.want)
 			}
 		})
 	}
